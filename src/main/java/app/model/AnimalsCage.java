5fcd25e4f3cb02be56bdb66ee88987f30b0f9573
Andrei Kryvaltsevich <krays170@gmail.com>
2023-01-02 15:29:38 +0300

    First commit

---
+++ src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,29 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.sql.Time;
+
+@Component
+public class AnimalsCage {
+
+    @Autowired
+    @Qualifier("Dog")
+    private Animal animal;
+
+    @Autowired
+    private Timer time;
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(animal.toString());
+        System.out.println("At:");
+        System.out.println(time.getTime());
+        System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return time;
+    }
+}
